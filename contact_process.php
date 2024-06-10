5d54c54f4b04c5ce566d5ec776f8c80ff1bfad3b
Istiak Ahmed <166763263+IstiakErik@users.noreply.github.com>
2024-06-10 13:29:47 +0600

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,44 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    // Recipient email address
+    $to = "istiakahmederik@gmail.com";
+    
+    // Form data
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
+    $subject = $_REQUEST['subject']; // This variable is set but not used in the email body
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // Email headers
+    $headers = "From: " . $from . "\r\n";
+    $headers .= "Reply-To: " . $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
+    // Email subject
     $subject = "You have a message from your Bitmap Photography.";
 
+    // Logo and link (adjust paths as needed)
     $logo = 'img/logo.png';
     $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+    // Email body
+    $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+    $body .= "<table style='width: 100%;'>";
+    $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+    $body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+    $body .= "</td></tr></thead><tbody><tr>";
+    $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+    $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+    $body .= "</tr>";
+    $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>"; // Correct variable used here
+    $body .= "<tr><td></td></tr>";
+    $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+    $body .= "</tbody></table>";
+    $body .= "</body></html>";
 
+    // Send email
     $send = mail($to, $subject, $body, $headers);
 
 ?>
